d7de63840ae94b06a278b212d569c78637d9598a
eric <contreras.eric18@gmail.com>
2021-08-06 15:44:23 -0400

    commit tree d5635cb3ab24e814aedf5069fe525bc9fd014c0b

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Add your functions here
+
+
+const map = (source, fn) => {
+    const newArray = [];
+    for(let i of source){
+        newArray.push(fn(i))
+    };
+    return newArray
+}
+
+const reduce = (source, fn, initialVal) => {
+    //check where to start, at initial or first array element if given initial
+    let value = (!!initialVal) ? initialVal : source[0];
+    let i = (!!initialVal) ? 0 : 1;
+
+    for(; i < source.length; i++){
+        value = fn(source[i], value);
+    }
+    return value
+}
